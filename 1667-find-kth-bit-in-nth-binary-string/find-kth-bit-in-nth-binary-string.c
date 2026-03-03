952a0f3a7779633a9fb0066f9325225d31571677
Ramchandnama <ramchandnama@gmail.com>
2026-03-03 22:12:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (30.00%) - LeetSync

---
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.c
@@ -0,0 +1,22 @@
+char findKthBit(int n, int k) {
+
+    if (n == 1) {
+        return '0';
+    }
+    
+   
+    int mid = 1 << (n - 1); 
+    
+    if (k == mid) {
+        
+        return '1';
+    } else if (k < mid) {
+        
+        return findKthBit(n - 1, k);
+    } else {
+     
+        int mirroredPos = mid - (k - mid);
+        char bit = findKthBit(n - 1, mirroredPos);
+        return (bit == '0') ? '1' : '0';
+    }
+}
