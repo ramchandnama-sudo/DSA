8bcbefade65514036fa98b51778ec0c1a74e23e5
Ramchandnama <ramchandnama@gmail.com>
2026-04-08 19:28:42 +0530

    Time: 62 ms (83.33%) | Memory: 25.8 MB (0.00%) - LeetSync

---
+++ 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.c
@@ -0,0 +1,17 @@
+int xorAfterQueries(int* nums, int numsSize, int** queries, int queriesSize, int* queriesColSize) {
+    long long mod = 1000000007;
+    for (int i = 0; i < queriesSize; i++) {
+        int l = queries[i][0];
+        int r = queries[i][1];
+        int k = queries[i][2];
+        int v = queries[i][3];
+        for (int idx = l; idx <= r; idx += k) {
+            nums[idx] = (int)((1LL * nums[idx] * v) % mod);
+        }
+    }
+    int ans = 0;
+    for (int i = 0; i < numsSize; i++) {
+        ans ^= nums[i];
+    }
+    return ans;
+}
